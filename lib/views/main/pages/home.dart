144196e27ba7d0d11d50ff1e114261cec4e15321
Lennart Koloska <lennart.koloska@stud.hs-hannover.de>
2021-05-23 17:54:32 +0200

    - Home - Overview : Context

--- lib/views/main/pages/home.dart
+++ lib/views/main/pages/home.dart
@@ -1,8 +1,10 @@
+import 'package:campus_motorsport/utils/size_config.dart';
 import 'package:campus_motorsport/widgets/general/layout/stacked_ui/context_drawer.dart';
 import 'package:campus_motorsport/widgets/general/layout/stacked_ui/main_view.dart';
 import 'package:campus_motorsport/widgets/home/feed.dart';
 import 'package:campus_motorsport/widgets/home/overview.dart';
 import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -49,22 +51,103 @@ class HomeContext extends StatefulWidget {
 }
 
 class HomeContextState extends State<HomeContext> {
-  String text = "Initial";
+  bool showMaintenances = true;
+  bool showEvents = true;
 
   @override
   Widget build(BuildContext context) {
     return ContextDrawer(
-      child: Center(
-          child: Text(
-        text,
-        style: Theme.of(context).textTheme.headline6,
-      )),
+      child: Material(
+        color: Colors.transparent,
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(10.0),
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                "Feed Filter",
+                style: Theme.of(context).textTheme.subtitle1,
+                textAlign: TextAlign.start,
+              ),
+              _buildFilterChips(context),
+              Divider(
+                thickness: 1.2,
+                indent: 10,
+                endIndent: 10,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              _buildInfo(context,
+                  "Wähle im Filter aus welche Arten von Aktivitäten angezeigt werden sollen.", LineIcons.filter),
+              const SizedBox(
+                height: 10,
+              ),
+              _buildInfo(context,
+                  "Tippe auf einen Eintrag im Feed um zur Detailansicht zu gelangen.", LineIcons.infoCircle),
+            ],
+          ),
+        ),
+      ),
     );
   }
 
-  void setText(String text) {
-    setState(() {
-      this.text = text;
-    });
+  Widget _buildFilterChips(BuildContext context) {
+    return Wrap(
+      spacing: 10,
+      runSpacing: 10,
+      children: <Widget>[
+        FilterChip(
+          label: Text(
+            'Wartungen',
+            style: getChipTextStyle(showMaintenances, context),
+          ),
+          selectedColor: Theme.of(context).colorScheme.primary,
+          selected: showMaintenances,
+          onSelected: (selected) {
+            setState(() {
+              showMaintenances = selected;
+            });
+          },
+        ),
+        FilterChip(
+          label: Text(
+            'Events',
+            style: getChipTextStyle(showEvents, context),
+          ),
+          selectedColor: Theme.of(context).colorScheme.primary,
+          selected: showEvents,
+          onSelected: (selected) {
+            setState(() {
+              showEvents = selected;
+            });
+          },
+        ),
+      ],
+    );
+  }
+
+  TextStyle? getChipTextStyle(bool selected, BuildContext context) {
+    return Theme.of(context).textTheme.bodyText2?.copyWith(
+        color: selected
+            ? Theme.of(context).colorScheme.onPrimary
+            : Theme.of(context).colorScheme.onSurface);
+  }
+
+  Widget _buildInfo(BuildContext context, String text, IconData icon) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Icon(icon),
+        Container(
+          width: SizeConfig.safeBlockHorizontal * 65,
+          child: Text(
+            text,
+          ),
+        ),
+      ],
+    );
   }
 }
